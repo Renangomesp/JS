bc3331788a8181a02d3cfba1553b75c591d4ce26
Renangomesp <renangomesst@gmail.com>
2025-07-28 19:21:06 -0300

    fix

--- aula12ex/modelo 01/script.js
+++ aula12ex/modelo 01/script.js
@@ -6,8 +6,46 @@ function verificar(){
     if (fano.value.length == 0 || Number(fano.value) > ano){
         window.alert(']ERRO] verifique os dados novamente')
     } else{
-       var fsex = document.getElementsByTagName('radsex')
-       var idade = ano - Number(fano.value) 
-       var  genero =
+        var fsex = document.getElementsByName ('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement ('img')
+        img.setAttribute ('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                img.setAttribute('src', 'h crianca.png')
+                //crianca
+            } else if (idade < 21){
+                img.setAttribute('src', 'm crianca.png')
+                //jovem
+            } else if (idade < 50){
+                 img.setAttribute('src', 'h jovem.png')
+                //adulto
+            } else{
+                 img.setAttribute('src', 'h idoso.png')
+                //idoso
+            }
+        } else if(fsex[1].checked) {
+            genero = 'Mulher'
+             if (idade >= 0 && idade < 10){
+                 img.setAttribute('src', 'm crianca.png')
+                //crianca
+            } else if (idade < 21){
+                 img.setAttribute('src', 'm jovem.png')
+                //jovem
+            } else if (idade < 50){
+                 img.setAttribute('src', 'm adulta.png')
+                //adulto
+            } else{
+                 img.setAttribute('src', 'm idosa.png')
+                //idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML= `detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
+
+
 }

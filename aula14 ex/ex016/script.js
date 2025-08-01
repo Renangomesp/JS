584be1b2167a1d354fd12c590ca154b7a07590e1
Renangomesp <renangomesst@gmail.com>
2025-08-01 18:47:02 -0300

    ex e assuntos

---
+++ aula14 ex/ex016/script.js
@@ -0,0 +1,29 @@
+function contar(){
+   var ini = document.getElementById('txti')
+   var fim = document.getElementById ('txtf')
+   var passo = document.getElementById ('txtp')
+   var res = document.getElementById ('res')
+   if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+     res.innerHTML = ('IMPOSSIVEL CONTAR')
+      //window.alert(' ERRO')
+   } else{
+      res.innerHTML = 'contando: <br>'
+      var i = Number(ini.value)
+      var f = Number(fim.value)
+      var p = Number(passo.value)
+      if (p <= 0){
+         window.alert('Passo Invalido Considerando PASSO 1')
+      }
+      if (i < f){
+         for(var c = i; c <= f; c+= p){
+         res.innerHTML += `${c} \u{1F603}`
+      }
+     
+      } else{
+         for(var c = i; c >= f; c -=p){
+          res.innerHTML += `${c} \u{1F603}`
+         }
+      }
+       res.innerHTML += ` \u{1F3C1}`
+   }
+}  
